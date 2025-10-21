f9af3006d503e2c6726918ad0b8ac005b73a1550
Starry <z5564122@ad.unsw.edu.au>
2025-10-21 21:10:38 +1100

    Refactor App class to initialize MessageManager and UserManager in the constructor

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,18 +3,26 @@ import { AuthManager } from "./auth/auth.js";
 import { PageController } from "./page-controller.js";
 import { ChannelManager } from "./features/channel-manager.js";
 import { Dashboard } from "./features/dashboard-manager.js";
+import { MessageManager } from "./features/message-manager.js";
+import { UserManager } from "./features/user-manager.js";
 
 export class App {
   constructor() {
     this.api = new ApiService();
     this.auth = new AuthManager();
     this.pageController = new PageController();
-    this.channel = new ChannelManager(this.api, this.auth, this.pageController);
-    this.dashboard = new Dashboard(this.api, this.auth, this.pageController, this.channel);
 
+    // Initialize managers
+    this.message = new MessageManager(this.api, this.auth, this.pageController);
+    this.user = new UserManager(this.api, this.auth, this.pageController);
+    this.channel = new ChannelManager(this.api, this.auth, this.pageController, this.message, this.user);
+    this.dashboard = new Dashboard(this.api, this.auth, this.pageController, this.channel);
   }
 
   init() {
+    // Initialize all managers
+    this.message.init();
+    this.user.init();
     this.dashboard.init();
   }
 
