535cb652401bdf5dae23a5ab10fb2971d4d0a088
Starry <z5564122@ad.unsw.edu.au>
2025-10-21 19:52:50 +1100

    Add AuthManager class to manage authentication tokens and user IDs

---
+++ frontend/src/auth/auth.js
@@ -0,0 +1,29 @@
+export class AuthManager {
+  constructor() {
+    this.TOKEN_KEY = "slackr_token";
+    this.USER_ID_KEY = "slackr_user_id";
+  }
+
+  saveAuthToken(token, userId) {
+    localStorage.setItem(this.TOKEN_KEY, token);
+    localStorage.setItem(this.USER_ID_KEY, userId.toString());
+  }
+
+  getToken() {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  getUserId() {
+    const userId = localStorage.getItem(this.USER_ID_KEY);
+    return userId ? parseInt(userId) : null;
+  }
+
+  checkLogin() {
+    return this.getToken() !== null;
+  }
+
+  cleanAuthToken() {
+    localStorage.removeItem(this.TOKEN_KEY);
+    localStorage.removeItem(this.USER_ID_KEY);
+  }
+}
